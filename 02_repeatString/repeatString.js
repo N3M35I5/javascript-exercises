4a86a8110ec88ed949ab0563ef5f80e34c225654
N3M35I5 <adwaith1998kv@gmail.com>
2022-05-22 14:26:49 +0530

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(stri,num) {
+    if(num==-1)
+    {
+        return 'ERROR'
+    }
+    res=''
+    for(let i=0;i<num;i++)
+    {
+        res+=stri
+    }
+    return res
 };
 
 // Do not edit below this line
