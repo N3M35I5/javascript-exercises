072a25446e054b41331ff1d282b5cf865c07e76c
N3M35I5 <adwaith1998kv@gmail.com>
2022-05-22 15:24:30 +0530

    Completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(stri) 
+{
+    let res=''
+    for(let i=stri.length-1;i>=0;i++)
+    {
+        res+=stri[i]
+    }
+    return res
 };
 
 // Do not edit below this line
