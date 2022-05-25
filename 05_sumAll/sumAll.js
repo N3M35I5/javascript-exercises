6a90e85752c6a0556232a963f50169536c9b04b2
N3M35I5 <adwaith1998kv@gmail.com>
2022-05-25 10:39:35 +0530

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(lowerLimit,upperLimit) 
+{
+    let sum=0;
+    let temp=0
+    if(upperLimit<0 || lowerLimit<0)
+        return "ERROR";
+    if(!(Number.isInteger(upperLimit)&&Number.isInteger(lowerLimit)))
+        return "ERROR"
+    if(upperLimit<lowerLimit)
+    {
+        temp=upperLimit;
+        upperLimit=lowerLimit;
+        lowerLimit=temp;
+    }
+    for(let i=lowerLimit;i<=upperLimit;i++)
+    {
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
