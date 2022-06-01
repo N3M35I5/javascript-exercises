43e11408f660a92e801bd7045b38d04c9a69c1ca
N3M35I5 <adwaith1998kv@gmail.com>
2022-06-01 09:47:05 +0530

    exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPerson=null;
+    const d = new Date();
+    people.forEach(person => {
+        if(!('yearOfDeath'  in person))
+            person.yearOfDeath=d.getFullYear();
+        let age=person.yearOfDeath-person.yearOfBirth
+        if(oldestPerson==null || age>(oldestPerson.yearOfDeath-oldestPerson.yearOfBirth))
+        {
+            oldestPerson=person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
