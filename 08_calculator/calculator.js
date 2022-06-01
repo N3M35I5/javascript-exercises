228bd632eb80961d0baddb36a51e586688f9d11e
N3M35I5 <adwaith1998kv@gmail.com>
2022-06-01 08:55:02 +0530

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  if(a.length<1)
+    return 0
+  const totalSum = a.reduce((total,b)=>{
+    return total +=b;
+  });
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const totalprod=arr.reduce((prod,a)=>{
+    return prod*= a;
+  });
+  return totalprod;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact=1;
+  if(a<=1)
+  {
+    return 1;
+  }
+  for(let i =1; i<=a;i++)
+  {
+    fact*=i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
