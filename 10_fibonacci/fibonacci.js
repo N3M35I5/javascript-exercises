14bd5071cb515a82b3aba482caca667f92144bbc
N3M35I5 <adwaith1998kv@gmail.com>
2022-06-01 09:27:53 +0530

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let a = 1;
+    let b = 1;
+    let c = 0;
+    num=Number(num);
+    if(num<0)
+    {
+        return "OOPS";
+    }
+    if(num<3){
+        return 1;
+    }
+    for(let i = 0; i<num-2;i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    return c;
 };
 
 // Do not edit below this line
